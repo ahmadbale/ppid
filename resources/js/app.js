42fd1408fba4c4a504ee9e1639f0f9b45bd932e2
Ahmad Iqbal Firmansyah <ahmedeqbal101@gmail.com>
2025-02-16 12:55:13 +0700

    alpine js

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,4 +2,5 @@ import './bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import Alpine from 'alpinejs';
 
