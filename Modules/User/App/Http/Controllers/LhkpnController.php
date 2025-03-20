bb3e6506c70c52f9af5386055ed4cb25729d86db
Ahmad Iqbal Firmansyah <ahmedeqbal101@gmail.com>
2025-03-20 13:13:08 +0700

    Merge Sisfo

--- Modules/User/App/Http/Controllers/LhkpnController.php
+++ Modules/User/App/Http/Controllers/LhkpnController.php
@@ -2,65 +2,102 @@
 
 namespace Modules\User\App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Http;
-use Illuminate\Support\Facades\Log;
 use App\Http\Controllers\Controller;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
-class LhkpnController extends Controller
+class LHKPNController extends Controller
 {
-    public function getLHKPNData()
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
     {
-        try {
-            Log::info('Memulai proses pengambilan data LHKPN');
-
-            $response = Http::get('http://ppid-polinema.test/api/public/getDataLhkpn');
-       
-            if ($response->failed() || !$response->json('success')) {
-                Log::warning('LHKPN API gagal diambil atau data tidak lengkap', [
-                    'response' => $response->json() ?? 'Tidak ada response'
-                ]);
-                return view('user::lhkpn', ['data' => [], 'error' => 'Gagal mengambil data LHKPN']);
-            }
-
-            Log::info('LHKPNController: Response API', ['response' => $response->json()]);
-      
-            $processedData = $this->processLHKPNData($response->json('data'));
-            return view('user::lhkpn', ['data' => $processedData, 'error' => null]);
-        } catch (\Exception $e) {
-            Log::error('LHKPN Data Fetch Error', [
-                'message' => $e->getMessage(),
-                'trace' => $e->getTraceAsString()
-            ]);
-            return view('user::lhkpn', ['data' => [], 'error' => 'Terjadi kesalahan saat mengambil data']);
-        }
-    }
-
-    private function processLHKPNData($data)
-    {
-        $result = [];
-
-        foreach ($data['data'] as $item) {
-            $result[] = [
-                'id' => $item['id'],
-                'tahun' => $item['tahun'],
-                'judul' => $item['judul'],
-                'deskripsi' => $item['deskripsi'],
-                'details' => array_map(function ($detail) {
-                    return [
-                        'id' => $detail['id'],
-                        'nama_karyawan' => $detail['nama_karyawan'],
-                        'file' => $detail['file']
-                    ];
-                }, $item['details']),
-                'total_karyawan' => $item['total_karyawan'],
-                'has_more' => $item['has_more']
-            ];
-        }
-
-        return [
-            'data' => $result,
-            'pagination' => $data['pagination']
+        // Data dasar hukum
+        $dasarHukum = [
+            'Peraturan Komisi Informasi Republik Indonesia Nomor 1 Tahun 2021 Pasal 15',
+            'Keputusan Direktur No. 1228 Tahun 2022 Butir 1'
         ];
+
+        // Data tahun tersedia
+        $tahunList = [2022, 2023];
+
+        // Tahun default
+        $tahunDipilih = $request->get('tahun', 2022);
+
+        // Data LHKPN berdasarkan tahun
+        $lhkpnData = [
+            2022 => [
+                ['nama' => 'Aang Afandi', 'link' => '#'],
+                ['nama' => 'Abdul Rasyid', 'link' => '#'],
+                ['nama' => 'Abdullah Helmy', 'link' => '#'],
+                ['nama' => 'Agus Suhardono', 'link' => '#'],
+                ['nama' => 'Ahmad Hermawan', 'link' => '#'],
+            ],
+            2023 => [
+                ['nama' => 'Bayu Saputra', 'link' => '#'],
+                ['nama' => 'Citra Dewi', 'link' => '#'],
+                ['nama' => 'Dian Pratama', 'link' => '#'],
+                ['nama' => 'Eko Suhendar', 'link' => '#'],
+                ['nama' => 'Fajar Santoso', 'link' => '#'],
+            ],
+        ];
+
+        // Kirim data ke view
+        return view('user::LHKPN', [
+            'dasarHukum' => $dasarHukum,
+            'tahunList' => $tahunList,
+            'tahunDipilih' => $tahunDipilih,
+            'lhkpnList' => $lhkpnData[$tahunDipilih] ?? []
+        ]);
     }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('user::create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    // public function store(Request $request): RedirectResponse
+    // {
+
+    // }
+
+    /**
+     * Show the specified resource.
+     */
+    // public function show($id)
+    // {
+    //     return view('user::show');
+    // }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    // public function edit($id)
+    // {
+    //     return view('user::edit');
+    // }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    // public function update(Request $request, $id): RedirectResponse
+    // {
+
+    // }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    // public function destroy($id)
+    // {
+    //     //
+    // }
 }
