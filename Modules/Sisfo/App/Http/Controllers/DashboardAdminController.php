54ebcb3557dc65d31caf3424e46d00444790b5cb
adeliashahahahaha <asha.syrn5@gmail.com>
2025-04-22 22:15:10 +0700

    redesign dashboard admin belom fix

--- Modules/Sisfo/App/Http/Controllers/DashboardAdminController.php
+++ Modules/Sisfo/App/Http/Controllers/DashboardAdminController.php
@@ -16,8 +16,69 @@ class DashboardAdminController extends Controller
         ];
 
         $activeMenu = 'dashboard';
+        // dummy
+        $permintaanTerbaru = [
+            [
+                'tanggal' => '2024-04-15',
+                'nama' => 'Rizky Ananda',
+                'status' => 'Pending',
+                'jenis' => 'Permohonan Informasi',
+            ],
+            [
+                'tanggal' => '2024-04-14',
+                'nama' => 'Nadia Fadhilah',
+                'status' => 'Pending',
+                'jenis' => 'WBS',
+            ],
+            [
+                'tanggal' => '2024-04-13',
+                'nama' => 'Andi Surya',
+                'status' => 'Diproses',
+                'jenis' => 'Pengaduan Masyarakat',
+            ],
+            [
+                'tanggal' => '2024-04-12',
+                'nama' => 'Dian Pratama',
+                'status' => 'Diproses',
+                'jenis' => 'Permohonan Perawatan Sarana',
+            ],
+            [
+                'tanggal' => '2024-04-12',
+                'nama' => 'Dian Pratama',
+                'status' => 'Ditolak',
+                'jenis' => 'Permohonan Perawatan Sarana',
+            ],
+        ];
 
-        return view('sisfo::dashboardADM', ['breadcrumb' => $breadcrumb, 'activeMenu' => $activeMenu]);
+        $menuBoxes = [
+            [
+                'jumlah' => 150,
+                'status' => 'Masuk',
+                'bg' => 'primary', // biru terang: cocok untuk masuk
+                'icon' => 'fas fa-inbox' // ikon masuk/inbox
+            ],
+            [
+                'jumlah' => 53,
+                'status' => 'Diproses',
+                'bg' => 'warning', // kuning: cocok untuk proses
+                'icon' => 'fas fa-spinner' // ikon loading/proses
+            ],
+            [
+                'jumlah' => 44,
+                'status' => 'Disetujui',
+                'bg' => 'success', // hijau: cocok untuk status positif
+                'icon' => 'fas fa-check-circle' // ikon centang
+            ],
+            [
+                'jumlah' => 65,
+                'status' => 'Ditolak',
+                'bg' => 'danger', // merah: cocok untuk status gagal
+                'icon' => 'fas fa-times-circle' // ikon silang/tolak
+            ],
+        ];
+
+
+        return view('sisfo::dashboardADM', ['breadcrumb' => $breadcrumb, 'activeMenu' => $activeMenu, 'permintaanTerbaru' => $permintaanTerbaru, 'menuBoxes' => $menuBoxes]);
 
     }
 }
