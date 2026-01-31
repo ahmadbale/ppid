402743a63118da9ddc022d88ea8115a7a0703193
gelbiasa <gelbifirmansyah12@gmail.com>
2026-02-01 00:40:33 +0700

    penyesuaian 6 main file pada controller

--- Modules/Sisfo/App/Http/Controllers/AdminWeb/Menumanagement/MenuManagementController.php
+++ Modules/Sisfo/App/Http/Controllers/AdminWeb/Menumanagement/MenuManagementController.php
@@ -9,6 +9,7 @@ use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Modules\Sisfo\App\Models\HakAkses\SetHakAksesModel;
 use Modules\Sisfo\App\Models\WebMenuGlobalModel;
 
@@ -34,6 +35,28 @@ class MenuManagementController extends Controller
         }
     }
 
+    // API Gateway - handle multiple actions via query parameter
+    public function getData(Request $request)
+    {
+        if (!$request->ajax() && !$request->wantsJson()) {
+            return redirect()->back();
+        }
+
+        $action = $request->query('action', 'list');
+        
+        switch ($action) {
+            case 'parent-menus':
+                return $this->handleGetParentMenus($request);
+                
+            case 'reorder':
+                return $this->handleReorder($request);
+                
+            case 'list':
+            default:
+                return $this->getMenuList();
+        }
+    }
+
     public function addData($hakAksesId)
     {
         try {
@@ -43,9 +66,13 @@ class MenuManagementController extends Controller
                 return redirect()->back()->with('error', $result['message']);
             }
 
-            return view('sisfo::adminweb.MenuManagement.set-menu', $result['data']);
+            // Return full page create view
+            return view('sisfo::AdminWeb.MenuManagement.create', $result['data']);
         } catch (\Exception $e) {
-            return redirect()->back()->with('error', 'Error loading set menu page: ' . $e->getMessage());
+            Log::error('Error in addData: ' . $e->getMessage());
+            Log::error('Stack trace: ' . $e->getTraceAsString());
+            
+            return redirect()->back()->with('error', 'Error loading create menu page: ' . $e->getMessage());
         }
     }
 
@@ -76,15 +103,6 @@ class MenuManagementController extends Controller
         return redirect()->back();
     }
 
-    public function deleteData($id)
-    {
-        if (request()->ajax()) {
-            $result = WebMenuModel::deleteData($id);
-            return response()->json($result);
-        }
-        return redirect()->back();
-    }
-
     public function detailData($id)
     {
         if (request()->ajax()) {
@@ -94,21 +112,36 @@ class MenuManagementController extends Controller
         return redirect()->back();
     }
 
-    public function reorder(Request $request)
+    public function deleteData(Request $request, $id)
     {
         if ($request->ajax()) {
-            $result = WebMenuModel::reorderMenus($request->get('data'));
+            $result = WebMenuModel::deleteData($id);
             return response()->json($result);
         }
         return redirect()->back();
     }
 
-    public function getParentMenus($hakAksesId)
+    // Private: Handle reorder request via getData API Gateway
+    private function handleReorder(Request $request)
     {
-        if (request()->ajax()) {
-            $result = WebMenuModel::getParentMenusData($hakAksesId, request()->input('exclude_id'));
-            return response()->json($result);
-        }
-        return redirect()->back();
+        $result = WebMenuModel::reorderMenus($request->input('data'));
+        return response()->json($result);
+    }
+
+    // Private: Handle get parent menus via getData API Gateway
+    private function handleGetParentMenus(Request $request)
+    {
+        $hakAksesId = $request->query('hak_akses_id');
+        $excludeId = $request->query('exclude_id');
+        
+        $result = WebMenuModel::getParentMenusData($hakAksesId, $excludeId);
+        return response()->json($result);
+    }
+
+    // Private: Default getData - return menu list
+    private function getMenuList()
+    {
+        $result = WebMenuModel::selectData();
+        return response()->json($result);
     }
 }
