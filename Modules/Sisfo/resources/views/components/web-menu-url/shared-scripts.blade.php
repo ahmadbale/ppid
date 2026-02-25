4dcd60e8925cb74f8dae7d23b096cdfdcc9348e7
gelbiasa <gelbifirmansyah12@gmail.com>
2026-02-25 22:20:20 +0700

    shared script

---
+++ Modules/Sisfo/resources/views/components/web-menu-url/shared-scripts.blade.php
@@ -0,0 +1,5 @@
+{{-- 
+  Component: Shared Scripts for Web Menu URL
+  Load shared.js file untuk create.blade.php dan update.blade.php
+--}}
+<script src="{{ asset('modules/sisfo/js/web-menu-url/shared.js') }}?v={{ time() }}"></script>
