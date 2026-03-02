b5af1c952e462f6f76ea744ba3d58a3b0eb8c5bf
gelbiasa <gelbifirmansyah12@gmail.com>
2026-03-02 20:55:41 +0700

    feat(menu-master): add shared scripts component for menu management

---
+++ Modules/Sisfo/resources/views/components/menu-master/shared-scripts.blade.php
@@ -0,0 +1,5 @@
+{{-- 
+  Component: Shared Scripts for Menu Master
+  Load shared.js untuk create.blade.php dan update.blade.php
+--}}
+<script src="{{ asset('modules/sisfo/js/menu-master/shared.js') }}?v={{ time() }}"></script>
