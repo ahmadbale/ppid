e35808fe96d3df0e61d54f5c78421fd89328a9bf
gelbiasa <gelbifirmansyah12@gmail.com>
2026-03-16 22:34:04 +0700

    feat(init): add shared scripts for web menu global component

---
+++ Modules/Sisfo/resources/views/components/web-menu-global/shared-scripts.blade.php
@@ -0,0 +1,5 @@
+{{-- 
+  Component: Shared Scripts for Web Menu Global
+  Load shared.js untuk create.blade.php dan update.blade.php
+--}}
+<script src="{{ asset('modules/sisfo/js/web-menu-global/shared.js') }}?v={{ time() }}"></script>
