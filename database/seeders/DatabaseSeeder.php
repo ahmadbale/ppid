3e6e91bc58c70d6ddf46ff1032d590aab732b8a0
gelbiasa <gelbifirmansyah12@gmail.com>
2026-03-25 22:11:18 +0700

    feat(seeder): add additional seeders for web menu and application data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,28 @@ class DatabaseSeeder extends Seeder
         $this->call([
             \Database\Seeders\Seeder000001MHakAkses::class,
             \Database\Seeders\Seeder000002MUser::class,
+            \Database\Seeders\Seeder000003WebMenuUrl::class,
+            \Database\Seeders\Seeder000004WebMenuGlobal::class,
+            \Database\Seeders\Seeder000005WebMenu::class,
             \Database\Seeders\Seeder000006SetHakAkses::class,
             \Database\Seeders\Seeder000007SetUserHakAkses::class,
+            \Database\Seeders\Seeder000009MApplication::class,
+            \Database\Seeders\Seeder000026MKategoriForm::class,
+            \Database\Seeders\Seeder000029MBeritaDinamis::class,
+            \Database\Seeders\Seeder000031MIpDinamisTabel::class,
+            \Database\Seeders\Seeder000032MKategoriAkses::class,
+            \Database\Seeders\Seeder000033MKategoriFooter::class,
+            \Database\Seeders\Seeder000034MLhkpn::class,
+            \Database\Seeders\Seeder000036MMediaDinamis::class,
+            \Database\Seeders\Seeder000037MPengumumanDinamis::class,
+            \Database\Seeders\Seeder000039MRegulasiDinamis::class,
+            \Database\Seeders\Seeder000040TAksesCepat::class,
+            \Database\Seeders\Seeder000042TDetailLhkpn::class,
+            \Database\Seeders\Seeder000043TDetailMediaDinamis::class,
+            \Database\Seeders\Seeder000046TFooter::class,
+            \Database\Seeders\Seeder000051TPintasanLainnya::class,
+            \Database\Seeders\Seeder000052TPengumuman::class,
+            \Database\Seeders\Seeder000054TKategoriRegulasi::class,
         ]);
     }
 }
